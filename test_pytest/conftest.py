fa1b2f5d4ed653c6387893b414ab751aac290920
Provod228 <tulush.ignat@bk.ru>
2025-01-15 23:54:58 +0300

    update test

--- test_pytest/conftest.py
+++ test_pytest/conftest.py
@@ -1,21 +1,21 @@
 import pytest
 import sqlite3
-import datetime
 from faker import Faker
 from random import randint
 from HERNY.main_herny.editing import User, Role, Status
 from HERNY.main_herny.create_table_db import create_user, create_role, create_status
 
 
-@pytest.fixture(scope='session')
-def test_db_connection(tmpdir_factory):
+@pytest.fixture(scope='function')
+def test_user_db_connection(tmpdir_factory):
+    fake = Faker()
     db_path = tmpdir_factory.mktemp("data").join("test.db")
     connection = sqlite3.connect(str(db_path))
     cursor = connection.cursor()
 
     create_user(cursor)
-
-    fake = Faker()
+    create_role(cursor)
+    create_status(cursor)
 
     for index in range(3, randint(4, 10)):
         User(
@@ -24,6 +24,8 @@ def test_db_connection(tmpdir_factory):
             time_add_user=fake.date(), time_status_change_user=fake.date(),
             status_user=1, role_user=1,
         ).add(cursor)
+        Status(name_status=f"test_status{index}").add(cursor)
+        Role(name_role=f"test_role{index}").add(cursor)
 
     cursor.execute("select * from User")
     connection.commit()
@@ -31,3 +33,42 @@ def test_db_connection(tmpdir_factory):
     yield cursor.fetchall()
     cursor.close()
     connection.close()
+
+
+@pytest.fixture(scope='function')
+def test_status_db_connection(tmpdir_factory):
+    db_path = tmpdir_factory.mktemp("data").join("test.db")
+    connection = sqlite3.connect(str(db_path))
+    cursor = connection.cursor()
+
+    create_status(cursor)
+
+    for index in range(3, randint(4, 10)):
+        Status(name_status=f"test_status{index}").add(cursor)
+
+    cursor.execute("select * from Status")
+    connection.commit()
+
+    yield cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+
+@pytest.fixture(scope='function')
+def test_role_db_connection(tmpdir_factory):
+    db_path = tmpdir_factory.mktemp("data").join("test.db")
+    connection = sqlite3.connect(str(db_path))
+    cursor = connection.cursor()
+
+    create_role(cursor)
+
+    for index in range(3, randint(4, 10)):
+        Role(name_role=f"test_role{index}").add(cursor)
+
+    cursor.execute("select * from Role")
+    connection.commit()
+
+    yield cursor.fetchall()
+    cursor.close()
+    connection.close()
+
